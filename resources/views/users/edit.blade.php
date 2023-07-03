b7e6d2236b48252fd85d703bd051948d1fc1b468
Om291202Pandey <omp.agt@gmail.com>
2023-07-03 06:17:42 -0700

    edit change of users

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -25,25 +25,54 @@
                 </div>
 
                 <div class="mb-3">
-                    <label for="name" class="form-label">Last Name</label>
-                    <input value="{{ $user->firstname }}" 
-                        type="text" 
-                        class="form-control" 
-                        name="lastname" 
-                        placeholder="last Name" required>
+                    {{-- <label for="name" class="form-label">Last Name</label> --}}
+                    <input type="hidden" name="hiddenField" value="1">
                     @if ($errors->has('lastname'))
                         <span class="text-danger text-left">{{ $errors->first('lastname') }}</span>
                     @endif
                 </div>
                 <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
+                    <label for="email" class="form-label">Employee Id</label>
                     <input value="{{ $user->email }}"
-                        type="email" 
+                        type="text" 
                         class="form-control" 
                         name="email" 
                         placeholder="Email address" required>
                     @if ($errors->has('email'))
                         <span class="text-danger text-left">{{ $errors->first('email') }}</span>
+                    @endif
+                </div>
+                <div class="mb-3">
+                    <label for="division" class="form-label">Division</label>
+                    <input value="{{ $user->division }}"
+                        type="text" 
+                        class="form-control" 
+                        name="division" 
+                        placeholder="Email address" required>
+                    @if ($errors->has('division'))
+                        <span class="text-danger text-left">{{ $errors->first('division') }}</span>
+                    @endif
+                </div>
+                <div class="mb-3">
+                    <label for="headquarter" class="form-label">Headquarter</label>
+                    <input value="{{ $user->headquarter }}"
+                        type="text" 
+                        class="form-control" 
+                        name="headquarter" 
+                        placeholder="Email address" required>
+                    @if ($errors->has('headquarter'))
+                        <span class="text-danger text-left">{{ $errors->first('headquarter') }}</span>
+                    @endif
+                </div>
+                <div class="mb-3">
+                    <label for="designer" class="form-label">Designer</label>
+                    <input value="{{ $user->designer }}"
+                        type="text" 
+                        class="form-control" 
+                        name="designer" 
+                        placeholder="Email address" required>
+                    @if ($errors->has('designer'))
+                        <span class="text-danger text-left">{{ $errors->first('designer') }}</span>
                     @endif
                 </div>
                 {{-- <div class="mb-3">
